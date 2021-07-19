519383a8e8a4ddff2cc92b49260163cf36c464ac
Stefano <stefano.funaro@gmail.com>
2021-07-19 16:26:12 +0200

    playing_with_CSS

--- js/main.js
+++ js/main.js
@@ -5,22 +5,33 @@
 // numbers from 1 to 100 print
 
 var iterationNumber = 100;
+var numberFizzBuzz = [];
+var numbers = document.getElementById("numbers");
+var buttons = document.getElementById("buttons");
 for (var i = 0; i < iterationNumber; i++) {   
     if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0)
     {
         // 3 and 5 multiples
-        console.log("FizzBuzz")
+        console.log("FizzBuzz");
+        numberFizzBuzz.push("FizzBuzz");
     } else if ( (i + 1) % 5 == 0)
     {
         // 5 multiples
         console.log("Buzz");
+        numberFizzBuzz.push("Buzz");
     } else if ( (i + 1) % 3 == 0)
     {
         // 3 multiples
         console.log("Fizz");
+        numberFizzBuzz.push("Fizz");
     } else 
     {
         //all the other numbers
         console.log( i + 1);
+        numberFizzBuzz.push(i + 1);
     }
+    numbers.innerHTML += "<p class='py-2'>" + numberFizzBuzz[i] + "</p>"
+    buttons
+    .innerHTML += "<p class='ms_button_decoration'>" + numberFizzBuzz[i] + "</p><br>"
 }
+console.log(numberFizzBuzz);
